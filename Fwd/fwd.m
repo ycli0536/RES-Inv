660687f173132776cc3375b05b4043358f97d246
ycli0536 <yinchu.li@hotmail.com>
2020-01-09 13:04:15 +0800

    fwd for ESS-HPC with 32 core

--- Fwd/fwd.m
+++ Fwd/fwd.m
@@ -1,5 +1,6 @@
 
 %% initial
+parpool(32);
 % Fracturing conductivity
 fracCon_list = [150 175 200 225 250];
 fracLoc = [300 300 -200 200 -1700 -2100];
@@ -42,19 +43,8 @@ center_dim2 = zc;
 % pseudo-random 8 control points detemining ploygon's shape
 r = n/2 * dz; % basical radius with 3sigma principle
 
-% savePath = '/home/liyinchu/Data/fwd_forloop/new_dataset20191021/';
-savePath = 'D:/Yinchu Li/EMG_largeFiles/forloop_noProductionWell/dataset20191219/';
-name_SheetCrossSection = 'SheetCrossSection';
-name_ObsData = 'ObsData';
-name_51X51_ObsData = '51X51_ObsData';
-
-path_SheetCrossSection = [savePath name_SheetCrossSection '/'];
-path_ObsData = [savePath name_ObsData '/'];
-path_51X51_ObsData = [savePath name_51X51_ObsData '/'];
-
-if exist(path_SheetCrossSection, 'dir') == 0; mkdir(path_SheetCrossSection); end
-if exist(path_ObsData, 'dir') == 0;           mkdir(path_ObsData);           end
-if exist(path_51X51_ObsData, 'dir') == 0;     mkdir(path_51X51_ObsData);     end
+savePath = '/share/home/liyinchu/DATA/fwd_forloop/dataset20200109/';
+if exist(savePath, 'dir') == 0; mkdir(savePath); end
 
 % (1) get connectivity
 [nodes, edges, lengths, faces, cells] = ...
@@ -74,14 +64,15 @@ ce = Edge2Edge * edgeCon; % on edges
 cc = Cell2Edge * cellCon; % on cells
 
 %% Import E_initial
-E_initial = load('E_initial.txt');
+Efield = load('E_initial_WellB.mat');
+E_initial = Efield.E_obs;
 
 Ex1 = E_initial(1:length(dataLoc_x));
 Ey1 = E_initial(length(dataLoc_y)+1:end);
 
 %% parfor producing data
 BatchSize = 1000;
-BatchNumber = 30;
+BatchNumber = 6;
 start_id = 1:BatchSize:1 + BatchNumber * BatchSize;
 % label = zeros(imax - imin + 1, 1);
 % data = zeros(imax - imin + 1, length(E.Mx));
@@ -182,61 +173,7 @@ label = [label; direction];
 end
 toc
 
-save([savePath 'Sheet#' num2str(fracCon) '_fracCon' num2str(k, '%02d') '.mat'], 'ShapeCollect', 'fracCons');
-save([savePath 'train#' num2str(fracCon) '_fracCon' num2str(k, '%02d') '.mat'], 'data', 'label');
+save([savePath 'Sheet#' num2str(k, '%02d') num2str(fracCon) '_fracCon' '.mat'], 'ShapeCollect', 'fracCons');
+save([savePath 'train#' num2str(k, '%02d') num2str(fracCon) '_fracCon' '.mat'], 'data', 'label');
 
 end
-
-% %% save data/figures
-% for i = 1:end_id
-%     
-% x = ShapeCollect(2 * i - 1, 1);
-% y = ShapeCollect(2 * i - 1, 2);
-% 
-% shape = figure('Visible', 'off');
-% plot([x; x(1)], [y; y(1)]);
-% hold on
-% axis([yc-dy*n yc+dy*n zc-dz*n zc+dz*n])
-% xticks(yc-dy*n : dy : yc+dy*n)
-% yticks(zc-dz*n : dz : zc+dz*n)
-% box on
-% axis square
-% grid on
-% set(gca, 'GridAlpha', 1, 'GridColor', [0.9 0.6 0.5])
-% line (well_locdim1, well_locdim2, 'Marker', 'x', 'MarkerSize', 12, 'MarkerEdgeColor', 'k');
-% line (well_locdim1, well_locdim2, 'Marker', 'o', 'MarkerSize', 12, 'MarkerEdgeColor', 'k');
-% title('Cross-section view')
-% saveas(shape, [path_SheetCrossSection, num2str(i,'%05d'), ...
-%     '_', char(label(i)), '_', name_SheetCrossSection, '.jpg'])
-% 
-% reso = 20;
-% cutoff = [1e-10 1e-6];
-% 
-% obs = figure('Visible', 'off');
-% [~, ~, ~, ZI] = imagexyc([dataLoc_x dataLoc_y data(i, 1:length(dataLoc_x)) data(i, length(dataLoc_y)+1:end)], reso,'',cutoff,'log');
-% hold on;
-% % levellist = -15:0.5:-3;
-% % contour(dataLocX, dataLocY, reshape(log10(sqrt(Fx.^2+ Fy.^2)),51,51),...
-% %         'showtext','on','levellist',levellist,'linecolor','k');
-% % xlabel('X(m)')
-% % ylabel('Y(m)')
-% % title('fwd result')
-% set(gca,'position',[0 0 1 1])
-% axis normal
-% axis off 
-% saveas(obs, [path_ObsData, num2str(i,'%05d'), ...
-%     '_', char(label(i)), '_' name_ObsData, '.jpg'])
-% imwrite(ZI, [path_51X51_ObsData, num2str(i,'%05d'), ...
-%     '_', char(label(i)), '_' name_51X51_ObsData, '.jpg'])
-% end
-% %% label data
-% status_U = movefile([path_51X51_ObsData '*_U_*.jpg'], [path_51X51_ObsData 'U\']);
-% status_UR = movefile([path_51X51_ObsData '*_UR_*.jpg'], [path_51X51_ObsData 'UR\']);
-% status_R = movefile([path_51X51_ObsData '*_R_*.jpg'], [path_51X51_ObsData 'R\']);
-% status_DR = movefile([path_51X51_ObsData '*_DR_*.jpg'], [path_51X51_ObsData 'DR\']);
-% status_D = movefile([path_51X51_ObsData '*_D_*.jpg'], [path_51X51_ObsData 'D\']);
-% status_DL = movefile([path_51X51_ObsData '*_DL_*.jpg'], [path_51X51_ObsData 'DL\']);
-% status_L = movefile([path_51X51_ObsData '*_L_*.jpg'], [path_51X51_ObsData 'L\']);
-% status_UL = movefile([path_51X51_ObsData '*_UL_*.jpg'], [path_51X51_ObsData 'UL\']);
-% status_None = movefile([path_51X51_ObsData '*_None_*.jpg'], [path_51X51_ObsData 'None\']);
-% 
