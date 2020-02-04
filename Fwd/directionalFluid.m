3af3911dbe02dced4637b02917b89004f4996415
ycli0536 <yinchu.li@hotmail.com>
2020-02-04 20:39:09 +0800

    update contour plotting details

--- Fwd/directionalFluid.m
+++ Fwd/directionalFluid.m
@@ -17,29 +17,31 @@ dataLoc_y = dataLocY(:);
 % load([dataPath 'E_upExp_WellA.mat'])
 % load([dataPath 'E_downExp_WellA.mat'])
 
-% dataPath = 'D:/data/forloop_noProductionWell/directionalFluid/';
-dataPath = 'D:/data/forloop_noProductionWell/directionalFluid_500ohm_base/';
+dataPath = 'D:/data/forloop_noProductionWell/directionalFluid/';
+% dataPath = 'D:/data/forloop_noProductionWell/directionalFluid_500ohm_base/';
 load([dataPath 'E_leftExp_WellB.mat'])
 load([dataPath 'E_rightExp_WellB.mat'])
 load([dataPath 'E_upExp_WellB.mat'])
 load([dataPath 'E_downExp_WellB.mat'])
 
-data = [E_obs_leftExp'; E_obs_rightExp'; E_obs_upExp'; E_obs_downExp'];
+data = [E_obs_upExp'; E_obs_downExp'; E_obs_leftExp'; E_obs_rightExp'];
 reso = 20;
 cutoff = [1e-12 1e-6];
 % figure; histogram(log10(abs(data)))
-for i = 1:5
+for i = 1:4
     figure;
     [~, ~, ~, ZI] = imagexyc([dataLoc_x dataLoc_y data(i, 1:length(dataLoc_x))' data(i, length(dataLoc_y)+1:end)'], ...
                          reso,'',cutoff,'log');
     hold on;
     levellist = -15:0.5:-3;
-    [ct, h] = contour(dataLoc.X, dataLoc.Y, reshape(log10(sqrt(data(1:length(dataLoc_x)).^2+ data(length(dataLoc_y)+1:end).^2)),51,51), 'linecolor','k');
-    clabel(ct,h, levellist, 'FontSize',15,'Color','k')
+    [ct, h] = contour(dataLocX, dataLocY, reshape(log10(sqrt(data(i, 1:length(dataLoc_x)).^2+ data(i, length(dataLoc_y)+1:end).^2)),51,51), 'linecolor','k');
+    clabel(ct,h, levellist, 'FontSize',14,'Color','k')
     clim = caxis;
-    c = colorbar('Ticks',[linspace(clim(1),clim(2),7)], 'TickLabels',{'-180','-120','-60','0','60','120','180'});
+    c = colorbar('Ticks',linspace(clim(1),clim(2),7), 'TickLabels',{'-180','-120','-60','0','60','120','180'});
     c.Label.String = 'degree';
     c.Label.Rotation = 0;
-    c.Label.Position = [0.5 -1.25 0];
+    c.Label.FontSize = 10;
+    c.Label.Units = 'normalized';
+    c.Label.Position = [0.5 1.06 0]; % c.Label.Position
     colormap(hsv)
 end
