a6a42da941497bda3ef453b0ac360003a139d868
ycli0536 <yinchu.li@hotmail.com>
2020-02-16 17:25:28 +0800

    update casing conductivity profiles generation and checking

--- Fwd/casing_profiles_plot.py
+++ Fwd/casing_profiles_plot.py
@@ -14,15 +14,35 @@ import os
 # [-plt] show certain profile
 
 
-def plot_casing_prof(images_range, num_segments, data):
-    depths = np.linspace(0, Maxdepth, 10000)
-    casingCon_prifle = np.empty_like(depths)
-    fot i in range():
+def plot_casing_prof(images_range, data, savePath):
+    for i in range(images_range):
+        # plot profile
+        # for j in range(num_segments[i, 0]-1, -1, -1):
+        #     casingCon = data[i][0][:, 6]
+        #     casingCon_prifle[np.where(depths >= data[i][0][j, 5])] = casingCon[j]
+        # plt.plot(casingCon_prifle, depths)
 
-        for j in range(num_segments[i, 0]-1, -1, -1):
-            casingCon = data[i][0][:, 6]
-            casingCon_prifle[np.where(depths >= data[0][0][j, 5])] = casingCon[j]
-            return 
+        depth = np.append(data[i][0][0, 5], data[i][0][:, 5])
+        casing_con = np.append(data[i][0][0, 6], data[i][0][:, 6])
+
+        fig, ax = plt.subplots(figsize=(2, 3))
+
+        ax.step(casing_con, depth, where='post')
+        # ax.plot(casing_con, depth, 'C0o', alpha=0.5)
+
+        # ax.set_ylim(-1500, 0)
+        # ax.set_xlim(300, 510)
+
+        ax.ticklabel_format(axis='x', style='sci', scilimits=(0,1))
+        ax.xaxis.get_offset_text().set_fontsize(8)
+        ax.tick_params(labelsize=8)
+
+        ax.set_xlabel('conductivity', fontsize=8)
+        ax.set_ylabel('depth (m)', fontsize=8)
+        ax.set_title('Conducitivity of caisng', fontsize=10)
+
+        ax.grid()
+        fig.savefig(os.path.join(savePath, '%05d' %i + "casing_profile.png"), dpi=300, bbox_inches='tight')
 
 
 def main():
@@ -30,8 +50,10 @@ def main():
                                      epilog="Yinchu Li 02/15/2020")
     parser.add_argument("filename", help="please input data file")
     # 2 ways: 1. with path; 2. without path
-    parser.add_argument("-s", "--save", action="store_true", help="save all or not")
-    parser.add_argument("-op", "--outputPATH", action="store_true", help="the path to put the profile images, default saving path is ./profiles")
+    parser.add_argument("-s", "--save", action="store_true",
+                        help="save all or not")
+    parser.add_argument("-op", "--outputPATH", action="store_true",
+                        help="the path to put the profile images, default saving path is ./profiles")
     args = parser.parse_args()
 
     dataPATH = os.path.abspath(args.filename)
@@ -41,21 +63,18 @@ def main():
     Maxdepth = min(data[0][0][:, 5])
     print('Max depth is: %.2fm' % Maxdepth)
 
-    num_segments = loadmat(dataPATH)["num_segments"]
     if args.save:
         if args.outputPATH:
-            plot_casing_prof(images_range=111,
-                             num_segments=num_segments,
+            plot_casing_prof(images_range=np.size(data),
                              data=data,
-                             savePath=os.path.abspath(arg.outputPATH))
+                             savePath=os.path.abspath(args.outputPATH))
         else:
-            savePath = os.path.join(os.getcwd, 'casing_Con_profiles')
+            savePath = os.path.join(os.getcwd(), 'casing_Con_profiles')
             if not os.path.isdir(savePath):
                 os.makedirs(savePath)
-            plot_casing_prof(images_range=111,
-                             num_segments=num_segments,
+            plot_casing_prof(images_range=np.size(data),
                              data=data,
-                             savePath=)
+                             savePath=savePath)
 
 
 if __name__ == "__main__":
