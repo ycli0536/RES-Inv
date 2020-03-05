d74a4c2dcc5814b6f200e8ea0027ad12f5c4cc00
ycli0536 <yinchu.li@hotmail.com>
2020-03-05 21:45:24 +0800

    1d profile data generation

--- Fwd/load_plot_casing.m
+++ Fwd/load_plot_casing.m
@@ -3,15 +3,6 @@ clear
 Config_file = 'ModelsDesign.ini';
 PATH = config_parser(Config_file, 'PATH');
 
-dataGridX = -500:20:500;
-dataGridY = -500:20:500;
-Ndata = length(dataGridX) * length(dataGridY);
-[dataLocX, dataLocY] = meshgrid(dataGridX, dataGridY);
-
-% for parfor
-dataLoc_x = dataLocX(:);
-dataLoc_y = dataLocY(:);
-
 dataPath = PATH.dataPath_PC;
 savePath = PATH.targetPath_PC;
 if exist(savePath, 'dir') == 0;     mkdir(savePath);     end
@@ -24,11 +15,71 @@ for i = 1:length(dataGrouplist)
 end
 
 reso = 20;
-cutoff = [1e-7 1e-2];
+cutoff = [1e-11 1e-2];
 % figure; histogram(log10(abs(data)))
-for i=1:size(data, 1)
-    [~, ~, ~, ZI] = imagexyc([dataLoc_x dataLoc_y data(i, 1:length(dataLoc_x))' data(i, length(dataLoc_y)+1:end)'], ...
-                         reso,'',cutoff,'log');
-    % test ZI
-    imwrite(ZI, [savePath, num2str(i,'%05d'), 'Ed_field_casingCon', '.png'])
+% loopplot_raw_imagexyc(savePath, data, cutoff)
+% loopplot_profile(savePath, data, cutoff)
+data_profile = data1D(savePath, data, 26, log10(cutoff));
+for i = 1:40
+    plot(data_profile(i,:))
+    grid on
+    hold on
 end
+
+function loopplot_raw_imagexyc(plotPath, data, cutoff)
+    dataGridX = -500:20:500;
+    dataGridY = -500:20:500;
+    [dataLocX, dataLocY] = meshgrid(dataGridX, dataGridY);
+    
+    dataLoc_x = dataLocX(:);
+    dataLoc_y = dataLocY(:);
+    
+    reso = 20;
+    for i = 1:size(data, 1)
+        [~, ~, ~, ZI] = imagexyc([dataLoc_x dataLoc_y data(i, 1:length(dataLoc_x))' data(i, length(dataLoc_y)+1:end)'], ...
+                         reso,'',cutoff,'log');
+        % test ZI
+        imwrite(ZI, [plotPath, num2str(i,'%05d'), 'Ed_field_casingCon', '.png'])
+    end
+end
+function loopplot_profile(plotPath, data, cutoff)
+    dataGridX = -500:20:500;
+    dataGridY = -500:20:500;
+    [dataLocX, dataLocY] = meshgrid(dataGridX, dataGridY);
+    
+    dataLoc_x = dataLocX(:);
+    dataLoc_y = dataLocY(:);
+    
+    k = 26;
+    for i = 1:size(data, 1)
+        amp = reshape(sqrt(data(i, 1:length(dataLoc_x)).^2+data(i, length(dataLoc_y)+1:end).^2),51,51);
+        figure_Aprofile(plotPath, dataLocX, data, amp, i, k, cutoff)
+    end
+end
+function data_output = data1D(savePath, data, k, cutoff)
+    dataGridX = -500:20:500;
+    dataGridY = -500:20:500;
+    [dataLocX, dataLocY] = meshgrid(dataGridX, dataGridY);
+    
+    dataLoc_x = dataLocX(:);
+    dataLoc_y = dataLocY(:);
+    data_output = [];
+    for i = 1:size(data, 1)
+        amp = reshape(sqrt(data(i, 1:length(dataLoc_x)).^2+data(i, length(dataLoc_y)+1:end).^2),51,51);
+        amp_log = log10(amp);
+        data_output = [data_output; amp_log(k, :)];
+    end
+    data_output(data_output <= cutoff(1)) = cutoff(1);
+    save([savePath 'Casing_profile#' num2str(k, '%02d') '_casingCon' '.mat'], 'data_output');
+end
+function figure_Aprofile(plotPath, dataLoc, data, amp, i, k, cutoff)
+    fig2 = figure;
+    set(fig2, 'Visible', 'off', 'Units', 'Normalized', 'OuterPosition', [0, 0, 0.4, 1])
+    semilogy(dataLoc(k, :), amp(k, :), '.-'); grid on %y = 0 profile
+    xlabel('X(m)')
+    ylabel('Amplitude')
+    ylim(cutoff)
+    title(['Amplitude profile (y = 0) - ' num2str(i) '/' num2str(size(data, 1))])
+    saveas(fig2, [plotPath '1D_amp' num2str(i) '.png'])
+end
+
