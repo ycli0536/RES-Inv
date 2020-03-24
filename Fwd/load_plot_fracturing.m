6949bfadd5021873e6198cd810977f51c2db4ada
ycli0536 <yinchu.li@hotmail.com>
2020-03-24 12:51:53 +0800

    batch plotting

--- Fwd/load_plot_fracturing.m
+++ Fwd/load_plot_fracturing.m
@@ -27,22 +27,35 @@ fig = figure; histogram(log10(abs(data)))
 set(fig, 'Visible', 'off')
 saveas(fig, [savePath, 'histogram_data_total', num2str(size(data, 1)), '.png'])
 cutoff = [1e-7 1e-2];
-loopplot_raw_imagexyc(plotPath, data, cutoff, fracCons, labels)
 
-function loopplot_raw_imagexyc(plotPath, data, cutoff, fracCons, labels)
-    dataGridX = -500:20:500;
-    dataGridY = -500:20:500;
-    [dataLocX, dataLocY] = meshgrid(dataGridX, dataGridY);
-    
-    dataLoc_x = dataLocX(:);
-    dataLoc_y = dataLocY(:);
-    
-    reso = 20;
-    for i = 1:size(data, 1) % parfor
+BatchNumber = 2;
+BatchSize = 100;
+
+loopplot_raw_imagexyc(plotPath, data, cutoff, fracCons, labels, BatchNumber, BatchSize)
+
+function loopplot_raw_imagexyc(plotPath, data, cutoff, fracCons, labels, BatchNumber, BatchSize)
+
+start_id = 1:BatchSize:1 + BatchNumber * BatchSize;
+
+dataGridX = -500:20:500;
+dataGridY = -500:20:500;
+[dataLocX, dataLocY] = meshgrid(dataGridX, dataGridY);
+
+dataLoc_x = dataLocX(:);
+dataLoc_y = dataLocY(:);
+
+reso = 20;
+
+for k = 1:BatchNumber
+    end_id = start_id(k) + BatchSize - 1;
+    for i = start_id(k):end_id % parfor
         [~, ~, ~, ZI] = imagexyc([dataLoc_x dataLoc_y data(i, 1:length(dataLoc_x))' data(i, length(dataLoc_y)+1:end)'], ...
-                         reso,'',cutoff,'log');
-        % test ZI
+                                 reso,'',cutoff,'log');
+        % imwrite
         imwrite(ZI, [plotPath, num2str(i,'%05d'), 'Ed_field_fracCon', num2str(fracCons(i)),...
                 '_', char(labels(i)), '.png'])
     end
+    disp(end_id)
 end
+end
+
