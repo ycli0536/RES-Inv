5b475249cb868a9aa2633791f7d384d02b4304a7
ycli0536 <yinchu.li@hotmail.com>
2020-05-14 14:19:41 +0800

    update 2d (fracturing) input data generation

--- Fwd/load_plot_fracturing.m
+++ Fwd/load_plot_fracturing.m
@@ -1,5 +1,5 @@
 % HPC terminal
-% switch parpool(32) and parfor
+% switch parpool(32 or 40) and parfor
 % savePath_PC -> savePath_HPC (including setup.m)
 % dataPath_PC -> dataPath_HPC
 % targetPath_PC -> targetPath_HPC
@@ -14,31 +14,39 @@ savePath = PATH.savePath_PC; % home dir
 dataPath = PATH.dataPath_PC; % E-field data path
 data_file = PATH.data_file; % directions and fracCon data
 
-plotPath = PATH.targetPath_PC; % images path
-if exist(plotPath, 'dir') == 0;     mkdir(plotPath);     end
+targetPath = PATH.targetPath_PC; % images path
+if exist(targetPath, 'dir') == 0;     mkdir(targetPath);     end
 
-load([savePath, data_file])
+shape_data = load([savePath, data_file]);
 
 dataGrouplist = dir([dataPath 'Fracturing' '*.mat']);
 
-labels = directions;
-fracCons = fracCon * ones(length(C), 1);
+labels = shape_data.directions;
+fracCons = shape_data.fracCon * ones(length(shape_data.C), 1);
 data = [];
 for i = 1:length(dataGrouplist)
     temp = load([dataPath dataGrouplist(i).name]);
     data = [data; temp.data];
 end
 
+% figure; histogram(log10(abs(data)))
 reso = 20;
 fig = figure; histogram(log10(abs(data)))
 set(fig, 'Visible', 'off')
-saveas(fig, [savePath, 'histogram_data_total', num2str(size(data, 1)), '.png'])
-cutoff = [10^(-11) 10^(-5.5)];
+saveas(fig, [targetPath, 'histogram_data_total', num2str(size(data, 1)), '.png'])
+cutoff = [1e-13 10^(-5.5)];
 
-BatchNumber = 2;
-BatchSize = 100;
+% log amp and [-1, 1] ang
+filename_orig = 'orig_fracCon_logAmp_Ang.mat';
+data2D_orig(targetPath, filename_orig, data)
 
-loopplot_raw_imagexyc(plotPath, data, cutoff, fracCons, labels, BatchNumber, BatchSize)
+% preprocessed: normalized [0, 1] log amp and [0, 1] ang
+filename_prep = 'fracCon_normalized_Amp_Ang.mat';
+data2D(targetPath, filename_prep, data, log10(cutoff))
+
+% BatchNumber = 2;
+% BatchSize = 100;
+% loopplot_raw_imagexyc(plotPath, data, cutoff, fracCons, labels, BatchNumber, BatchSize)
 
 function loopplot_raw_imagexyc(plotPath, data, cutoff, fracCons, labels, BatchNumber, BatchSize)
 
@@ -65,4 +73,53 @@ for k = 1:BatchNumber
     disp(end_id)
 end
 end
+
+
+function data2D_orig(savePath, filename, data)
+    dataGridX = -500:20:500;
+    dataGridY = -500:20:500;
+    [dataLocX, dataLocY] = meshgrid(dataGridX, dataGridY);
+    
+    dataLoc_x = dataLocX(:);
+    dataLoc_y = dataLocY(:);
+    data_log_amp_orig = zeros(size(data, 1), 51, 51);
+    data_log_ang_orig = zeros(size(data, 1), 51, 51);
+    for i = 1:size(data, 1)
+        Ex = data(i, 1:length(dataLoc_x));
+        Ey = data(i, length(dataLoc_y)+1:end);
+        amp = reshape(sqrt(Ex.^2+Ey.^2),51,51);
+        amp_log = log10(amp);
+        ang = atan2(Ex, -Ey) / pi; % range from -1 to 1
+        data_log_amp_orig(i, :, :) = amp_log;
+        data_log_ang_orig(i, :, :) = reshape(ang, 51, 51);
+    end
+    save([savePath 'orig_' filename], 'data_log_amp_orig', 'data_log_ang_orig');
+end
+
+function data2D(savePath, filename, data, cutoff)
+    dataGridX = -500:20:500;
+    dataGridY = -500:20:500;
+    [dataLocX, dataLocY] = meshgrid(dataGridX, dataGridY);
+    
+    dataLoc_x = dataLocX(:);
+    dataLoc_y = dataLocY(:);
+    data_log_amp = zeros(size(data, 1), 51, 51);
+    data_log_ang = zeros(size(data, 1), 51, 51);
+    for i = 1:size(data, 1)
+        Ex = data(i, 1:length(dataLoc_x));
+        Ey = data(i, length(dataLoc_y)+1:end);
+        amp = reshape(sqrt(Ex.^2+Ey.^2),51,51);
+        amp_log = log10(amp);
+        amp_log(amp_log <= cutoff(1)) = cutoff(1);
+        ampmin = min(min(amp_log));
+        ampmax = max(max(amp_log));
+        amp_log = interp1([ampmin ampmax],[0 1],amp_log);
+        ang = atan2(Ex, -Ey) / pi; % range from -1 to 1
+        ang = interp1([-1 1],[0 1],ang);
+        data_log_amp(i, :, :) = amp_log;
+        data_log_ang(i, :, :) = reshape(ang, 51, 51);
+    end
+    save([savePath filename], 'data_log_amp', 'data_log_ang');
+end
+
 
