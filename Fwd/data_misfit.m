38d1b55d4d198c8aa2cb36be02f5f10347c3c663
ycli0536 <yinchu.li@hotmail.com>
2020-05-25 14:55:48 +0800

    add relative different of data_misfit

--- Fwd/data_misfit.m
+++ Fwd/data_misfit.m
@@ -11,14 +11,14 @@ PATH = config_parser(Config_file, 'PATH');
 savePath = PATH.savePath_PC; % home dir
 testPath = PATH.testPath_PC; % test and pred dataset E-field data path
 
-test_dataGrouplist = dir([testPath PATH.data_prefix '*.mat']);
+test_dataGrouplist = dir([testPath 'test_' PATH.data_prefix '*.mat']);
 test_data = [];
 for i = 1:length(test_dataGrouplist)
     temp = load([testPath test_dataGrouplist(i).name]);
     test_data = [test_data; temp.data];
 end
 
-pred_dataGrouplist = dir([testPath PATH.data_prefix '*.mat']);
+pred_dataGrouplist = dir([testPath 'pred_' PATH.data_prefix '*.mat']);
 pred_data = [];
 for i = 1:length(pred_dataGrouplist)
     temp = load([testPath pred_dataGrouplist(i).name]);
@@ -27,6 +27,8 @@ end
 
 eps = 0.05;
 misfit5 = zeros(size(test_data, 1), 1);
+relative_diff = zeros(size(test_data, 1), 1);
+rmse = zeros(size(test_data, 1), 1);
 for i = 1: size(test_data, 1)
     dE0 = test_data(i, :);
     dEx = test_data(i, 1:size(test_data, 2)/2);
@@ -35,5 +37,10 @@ for i = 1: size(test_data, 1)
     
     dE1 = pred_data(i, :);
     ddE = dE1 - dE0;
-    misfit5(i) = sum((ddE./(E0_amp.*eps)).^2);
+    misfit5(i) = sum((ddE ./ (E0_amp .* eps)).^2);
+    relative_diff(i) = sum(abs(ddE ./ dE0)) / length(ddE);
+    rmse(i) = sqrt(sum((dE1 - dE0).^2) / length(ddE));
 end
+
+[misfit_sorted, id_misfit] = sort(misfit5);
+[relative_diff_sorted, id_rel_diff] = sort(relative_diff);
